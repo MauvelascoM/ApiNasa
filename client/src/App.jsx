6b72859947cf4e4e13b7fb0d8f9281c769a66505
MauvelascoM <2024605@student.cct.ie>
2025-06-26 13:20:40 +0100

    Adding APOD API from nasa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,41 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [apod, setApod] = useState(null);
 
   useEffect(() => {
-    fetch('https://your-backend-url.onrender.com/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
+    axios.get('https://your-backend-url.onrender.com/api/apod')
+      .then(res => setApod(res.data))
       .catch(console.error);
   }, []);
 
+  if (!apod) return <p>Loading...</p>;
+
   return (
     <div className="App">
-      <h1>NASA App</h1>
-      <p>{message}</p>
+      <h1>{apod.title}</h1>
+      <p>{apod.date}</p>
+      {apod.media_type === 'image' ? (
+        <img src={apod.url} alt={apod.title} width="600" />
+      ) : (
+        <iframe src={apod.url} width="600" height="400" title="NASA Video" />
+      )}
+      <p>{apod.explanation}</p>
+       {/* <div className="section">
+        <NearEarthObjects />
+      </div>
+
+      <div className="section">
+        <EarthImage />
+      </div>
+      <div className="section">
+        <Mars />
+      </div> */}
     </div>
+    
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
+const NASA_API_KEY = "doeOrDrZkTaYZQKoT0klkRa3ojksk4xmWI7bqcjN";
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
+});
+
+app.get('/api/apod', async (req, res) => {
+  try {
+    const { date } = req.query;
+    const response = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}&date=${date || ''}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch APOD' });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
